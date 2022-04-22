b589b177c7272b9beb7273de0d728cd463290961
Benjamin Paba <pabagonzalezbenjamin@gmail.com>
2022-04-22 17:10:12 -0500

    testing GiftItem

---
+++ src/test/GiftItem.test.js
@@ -0,0 +1,14 @@
+import "@testing-library/jest-dom";
+
+describe('Pruebas en el Componente GifItem', () => { 
+    
+    test('Prueba que se renderice correctamente', () => { 
+        /* utilizaremos la libreria enzyme, la cual ademas de hacer las pruebas en componentes mas facilmente,
+         nos genera capturas snapshoot de la estructura del componente y la compara con el resultado esperado
+        
+        */
+
+         
+
+     })
+ })
